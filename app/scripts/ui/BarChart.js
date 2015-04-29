514c9d484d91dd3961c80ca5b42a10cd8d8608eb
Arian Hatefi <hatefi.arian@gmail.com>
2015-04-28 22:05:11 -0700

    Use NVD3 for the bar chart

--- app/scripts/ui/BarChart.js
+++ app/scripts/ui/BarChart.js
@@ -1,23 +1,10 @@
-var c3 = require('c3');
-
-var sampleData = [
-  {
-    name: "java",
-    values: [100, 120]
-  },
-  {
-    name: "javascript",
-    values: [20, 40]
-  },
-  {
-    name: "react",
-    values: [50, 25]
-  },
-];
+var nvd3 = require('nvd3');
 
 var BarChart = function() {
-  var chart;
   var chartContainer;
+  var chart;
+
+  var percentageFormat = function(d) { return d + '%'; };
 
   function getChangePercentage(values) {
     var startCount = values[0];
@@ -29,64 +16,71 @@ var BarChart = function() {
     return [originalDataObj.name, getChangePercentage(originalDataObj.values)];
   }
 
+  function formatForNvd3(originalDataObj) {
+    return {
+      label: originalDataObj.name,
+      value: getChangePercentage(originalDataObj.values)
+    };
+  }
+
   function create(el) {
     chartContainer = d3.select(el);
-    chart = c3.generate({
-      bindto: chartContainer,
-      data: {
-        columns: [],
-        type: 'bar'
-      },
-      bar: {
-        width: {
-          ratio: 0.5
-        }
-      },
-      axis: {
-        x: {
-          show: false
-        },
-        y: {
-          tick: {
-            format: function(x) { return x + '%'; }
-          }
-        }
-      },
-      grid: {
-        y: {
-          show: true
-        }
-      }
-    });
+    nv.addGraph(function() {
+      chart = nv.models.discreteBarChart()
+                    .margin({top: 10, right: 10, bottom: 10, left: 85})
+                    .x(function(d) { return d.label;  })
+                    .y(function(d) { return d.value;  })
+                    .staggerLabels(true)
+                    .tooltips(true)
+                    .showValues(true)
+                    .transitionDuration(350)
+                    ;
 
-    // For testing:
-    // chart.load({columns: sampleData.map(formatForC3)});
-    updateTitle('Test Title');
+      chart.yAxis
+           .showMaxMin(false)
+           .axisLabel('Percent Change')
+           .tickFormat(percentageFormat);
+
+      chartContainer.append('svg')
+                    .datum([])
+                    .call(chart)
+                    ;
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
   }
 
   function updateTitle(newTitle) {
-    var title = chartContainer.select('svg')
-      .selectAll('.title')
-      .data([newTitle], function(x) { return x; });
+    var svg = chartContainer.select('svg');
+    var title = svg.selectAll('.title')
+                   .data([newTitle], function(x) { return x; });
     title.enter()
-      .append('text')
-      .attr('x', chartContainer.select('svg').node().getBoundingClientRect().width / 2)
-      .attr('y', 16)
-      .attr('text-anchor', 'middle')
-      .style('font-size', '1.4em')
-      .text(function(d) { return d; })
-      ;
+         .append('text')
+         .attr('class', 'title')
+         .attr('x', chartContainer.select('svg').node().getBoundingClientRect().width / 2)
+         .attr('y', 16)
+         .attr('text-anchor', 'middle')
+         .style('font-size', '1.4em')
+         .text(function(d) { return d; })
+         ;
     title.exit().remove();
   }
 
   function update(data) {
-    chart.load({
-        columns: data.map(formatForC3).sort(function(a, b) {
-          return Math.abs(b[1]) - Math.abs(a[1]);
-        }).slice(0, 10)
+    nv.addGraph(function() {
+      chartContainer.select('svg')
+                    .datum([{
+                      values: data.map(formatForNvd3).sort(function(a, b) {
+                        return Math.abs(b.value) - Math.abs(a.value);
+                      }).slice(0, 10)
+                    }])
+                    .call(chart)
+                    ;
+      return chart;
     });
-
-    //updateTitle(newTitle);
+    //setTimeout(function() { updateTitle(newTitle); }, 1000);
   }
 
   return {
